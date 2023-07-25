c5bba4abc86197f5df38bda4198a7243dc6bbbda
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-07-24 18:59:14 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,30 +1,16 @@
 #include "main.h"
-
 /**
- * Description: prints a string
- * On success: returns no error
-*/
-
-void _puts(char *str)
-{
-	int i = 0;
-	while (*(str + i) != '\0')
-	{
-		putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
-}
-root@fbf02a54c483:/alx-low_level_programming/0x05-pointers_arrays_s
-root@fbf02a54c483:/alx-low_level_programming/0x05-pointers_arrays_s
-#include <stdio.h>
-#include <string.h>
-#include "main.h"
-
-/**
- *print_rev - Prints a string in reverse order
- *@s: String to reverse
- *Return: Nothing
+* print_rev - prints a string in reverse
+* @s: string to print
 */
 
 void print_rev(char *s)
+{
+int len = 0, index = 0;
+len = _strlen(s);
+
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
+
+_putchar('\n');
+}
