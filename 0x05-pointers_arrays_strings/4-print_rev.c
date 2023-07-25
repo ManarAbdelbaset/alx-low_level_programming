3744844b93acc3c0177253206e38309959c8a6a5
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-07-24 17:31:15 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
-* print_rev - prints a string in reverse
-* @s: string to print
+ * Description: prints a string
+ * On success: returns no error
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
+root@fbf02a54c483:/alx-low_level_programming/0x05-pointers_arrays_s
+root@fbf02a54c483:/alx-low_level_programming/0x05-pointers_arrays_s
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *print_rev - Prints a string in reverse order
+ *@s: String to reverse
+ *Return: Nothing
 */
 
 void print_rev(char *s)
-{
-int len = 0, index = 0;
-len = _strlen(s);
-
-for (index = len - 1; index >= 0; index--)
-_putchar(s[index]);
-
-_putchar('\n');
-}
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
-}
