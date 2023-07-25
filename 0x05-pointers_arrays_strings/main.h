2f94fc52b59a041f04065b64a8e4fa64079f2d4c
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-07-25 02:59:48 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,8 +4,9 @@
 /**                                                                                                                                   
 * void prototypes()                                                                                                                   
 * int prototypes()                                                                                                                    
-*/                                                                                                                                    
-                                                                                                                                      
+*/
+
+int _putchar(char c);
 void reset_to_98(int *n);                                                                                                             
 void swap_int(int *a, int *b);                                                                                                        
 int _strlen(char *s);                                                                                                                 
