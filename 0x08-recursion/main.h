6f79f23293eec402d32b85c6f9eb439d3da5d0e5
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-08-02 13:56:46 -0700

    101-wildcmp.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,6 @@ int is_prime(int n, int start);
 int is_palindrome(char *s);
 int check_palindrome(char *s, int len, int index);
 int find_strlen(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
