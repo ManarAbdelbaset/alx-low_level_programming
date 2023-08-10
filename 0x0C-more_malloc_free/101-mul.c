94b1932635151749a4aa8e912f4f16e3cd92a805
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-08-10 15:46:30 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -177,22 +177,54 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 }
 
 /**
- * main - print the result of the multiplication, followed by a new line
- * @argv: list
- * @argc: int
+ * main - Multiplies two positive numbers.
+ * @argv: The number of arguments passed to the program.
+ * @argc: An array of pointers to the arguments.
+ *
+ * Description: If the number of arguments is incorrect or one number
+ *              contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	char *final_prod, *next_prod;
+	int size, index, digit, zeroes = 0;
 
-	if (arge != 3)
+	if (argc != 3)
 	{
-		_puts("Error ");
+		printf("Error\n");
 		exit(98);
 	}
-	print_int(_atoi(argv[1] * _atio(argv[2]));
-			_putchar('\n');
 
-			return (0);
+	if (*(argv[1]) == '0')
+		argv[1] = iterate_zeroes(argv[1]);
+	if (*(argv[2]) == '0')
+		argv[2] = iterate_zeroes(argv[2]);
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	size = find_len(argv[1]) + find_len(argv[2]);
+	final_prod = create_xarray(size + 1);
+	next_prod = create_xarray(size + 1);
+
+	for (index = find_len(argv[2]) - 1; index >= 0; index--)
+	{
+		digit = get_digit(*(argv[2] + index));
+		get_prod(next_prod, argv[1], digit, zeroes++);
+		add_nums(final_prod, next_prod, size - 1);
+	}
+	for (index = 0; final_prod[index]; index++)
+	{
+		if (final_prod[index] != 'x')
+			putchar(final_prod[index]);
+	}
+	putchar('\n');
+
+	free(next_prod);
+	free(final_prod);
+
+	return (0);
 }
