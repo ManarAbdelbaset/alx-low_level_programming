c39bb53c10aa5ad34ebfd0a40d318d62fc5e8e7d
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-08-11 09:08:10 -0700

    101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,6 +12,7 @@ int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
+
 /**
  * _strlen - returns the length of a string
  * @s: the string whose length to check
@@ -21,11 +22,12 @@ int _isdigit(int c)
 int _strlen(char *s)
 {
 	int i = 0;
-	
+
 	while (*s++)
 		i++;
 	return (i);
 }
+
 /**
  * big_multiply - multiply two big number strings
  * @s1: the first big number string
@@ -37,7 +39,7 @@ char *big_multiply(char *s1, char *s2)
 {
 	char *r;
 	int l1, l2, a, b, c, x;
-	
+
 	l1 = _strlen(s1);
 	l2 = _strlen(s2);
 	r = malloc(a = x = l1 + l2);
@@ -45,6 +47,7 @@ char *big_multiply(char *s1, char *s2)
 		printf("Error\n"), exit(98);
 	while (a--)
 		r[a] = 0;
+
 	for (l1--; l1 >= 0; l1--)
 	{
 		if (!_isdigit(s1[l1]))
@@ -54,6 +57,7 @@ char *big_multiply(char *s1, char *s2)
 		}
 		a = s1[l1] - '0';
 		c = 0;
+
 		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
 		{
 			if (!_isdigit(s2[l2]))
@@ -62,8 +66,10 @@ char *big_multiply(char *s1, char *s2)
 				printf("Error\n"), exit(98);
 			}
 			b = s2[l2] - '0';
+
 			c += r[l1 + l2 + 1] + (a * b);
 			r[l1 + l2 + 1] = c % 10;
+
 			c /= 10;
 		}
 		if (c)
@@ -71,6 +77,8 @@ char *big_multiply(char *s1, char *s2)
 	}
 	return (r);
 }
+
+
 /**
  * main - multiply two big number strings
  * @argc: the number of arguments
@@ -82,9 +90,10 @@ int main(int argc, char **argv)
 {
 	char *r;
 	int a, c, x;
-	
+
 	if (argc != 3)
 		printf("Error\n"), exit(98);
+
 	x = _strlen(argv[1]) + _strlen(argv[2]);
 	r = big_multiply(argv[1], argv[2]);
 	c = 0;
