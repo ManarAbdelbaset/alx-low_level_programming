b5b925404534ec6b61890d719da4a8c76fbb4a6f
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-11-01 13:52:34 -0700

    101-str

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,34 +11,40 @@ char **strtow(char *str)
 {
 	char **words;
 	char *token;
+	char *str_copy;
 	int i = 0;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
 
-	words = malloc(sizeof(char *) * (strlen(str) + 1));
-	if (words == NULL)
+	str_copy = strdup(str);
+	if (str_copy == NULL)
 		return (NULL);
 
-	token = strtok(str, " ");
+	words = malloc(sizeof(char *) * (strlen(str_copy) + 1));
+	if (words == NULL)
+	{
+		free(str_copy);
+		return (NULL);
+	}
+
+	token = strtok(str_copy, " ");
 	while (token != NULL)
 	{
-		words[i] = malloc(sizeof(char) * (strlen(token) + 1));
+		words[i] = strdup(token);
 		if (words[i] == NULL)
 		{
-			while (i >= 0)
-			{
-				i--;
+			for (; i >= 0; i--)
 				free(words[i]);
-			}
 			free(words);
+			free(str_copy);
 			return (NULL);
 		}
-		strcpy(words[i], token);
 		i++;
 		token = strtok(NULL, " ");
 	}
 	words[i] = NULL;
 
+	free(str_copy);
 	return (words);
 }
