fdae41a0bdf4010b1f88f89dfe3b75e208b68796
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-11-01 13:10:34 -0700

    0-

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best
-			hacker\"\n\t- Kevin Mitnick");
+	/* Print a quote by Kevin Mitnick */
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t"
+			"- Kevin Mitnick");
 	return (0);
 }
