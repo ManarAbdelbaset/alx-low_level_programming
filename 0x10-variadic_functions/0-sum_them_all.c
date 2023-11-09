1c4922a6c4b2b5c05271bde30bcb2fc72b1e251e
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-11-09 11:54:01 -0800

    0-sum_them_all.c,1-print_numbers.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: number of arguments passed
+ *
+ * Return: sum of all parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i, sum = 0;
+
+	/* initialize valist for n number of arguments */
+	va_start(valist, n);
+
+	/* access all the arguments assigned to valist */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+
+	/* clean memory reserved for valist */
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+Solution:
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: separator to print between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: no return value (void)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	/* initialize valist for n number of arguments */
+	va_start(valist, n);
+
+	/* access all the arguments assigned to valist */
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	/* clean memory reserved for valist */
+	va_end(valist);
+}
