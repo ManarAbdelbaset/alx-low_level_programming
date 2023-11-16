c9fdd553a21d41ae2da08b64f44a9327f3d807dc
ManarAbdelbaset <manarabdelbaset135@gmail.com>
2023-11-16 13:45:21 -0800

    5,6

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - Prints the message before the main function is executed
+ */
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    ; Prepare the arguments for printf
+    mov rdi, format
+    mov rsi, hello
+    xor rax, rax
+
+    ; Call printf
+    call printf
+
+    ; Exit the program
+    mov eax, 0
+    ret
